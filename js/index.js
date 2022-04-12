fbd0630f89d380988282acaa42594965c258211f
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-11 20:07:39 -0500

    Add speakers
    Add speakers dynamically.

---
+++ js/index.js
@@ -0,0 +1,35 @@
+import { speakers, speakerTemplate } from './speakers.js';
+
+const speakersList = document.querySelector('#speakers-list');
+const moreSpeakers = document.querySelector('#all-speakers');
+let moreSpeakersToggle = false;
+
+function showSpeakers(allowSpeakers) {
+  let i = 0;
+  Object.values(speakers).every((speaker) => {
+    if (i > allowSpeakers) {
+      return false;
+    }
+    speakersList.appendChild(speakerTemplate(speaker));
+    i += 1;
+    return true;
+  });
+}
+
+moreSpeakers.addEventListener('click', () => {
+  speakersList.innerHTML = '';
+  if (moreSpeakersToggle) {
+    showSpeakers(1);
+    document.querySelector("[class~=btn-more]").innerHTML = 'More';
+    document.querySelector("[class~=rotate]").classList.remove('pi');
+    document.querySelector("[class~=rotate]").setAttribute('viewBox', '-6 -6 32 32');
+  } else {
+    showSpeakers(Object.keys(speakers).length);
+    document.querySelector("[class~=btn-more]").innerHTML = 'Less';
+    document.querySelector("[class~=rotate]").classList.add('pi');
+    document.querySelector("[class~=rotate]").setAttribute('viewBox', '-6 -10 32 32');
+  }
+  moreSpeakersToggle = !moreSpeakersToggle;
+});
+
+showSpeakers(1);
